fd5c7888052d9488d285f170995535e658bf8a7b
Baran Şahin <barana340@gmail.com>
2022-07-03 00:08:27 +0300

    updated papatya

--- papatya.py
+++ papatya.py
@@ -80,19 +80,39 @@ def normalize(x):
 	This function takes a numpy array or arraylike element as parameter and normalize its values to range[0,1], and returns
 	result as numpy array.
 	'''
-	min_val = x[0]
-	max_val = x[0]
-	for i in x:
-		if(i < min_val):
-			min_val = i
-		if(i > max_val):
-			max_val = i
-	
-	new_x = []
-	for i in x:
-		new_x.append(float(i-min_val)/float(max_val-min_val))
-	
-	return np.asarray(new_x)
+	if(len(x.shape) == 1):
+		min_val = x[0]
+		max_val = x[0]
+		for i in x:
+			if(i < min_val):
+				min_val = i
+			if(i > max_val):
+				max_val = i
+		
+		new_x = []
+		for i in x:
+			new_x.append(float(i-min_val)/float(max_val-min_val))
+		
+		return np.asarray(new_x)
+	elif(len(x.shape) == 2):
+		new_matrix = []
+		for column_index in range(x.shape[1]):
+			min_val = x[0][column_index]
+			max_val = x[0][column_index]
+			for i in x:
+				if(i[column_index] < min_val):
+					min_val = i[column_index]
+				if(i[column_index] > max_val):
+					max_val = i[column_index]
+			new_column = []
+			for i in x:
+				new_column.append(float(i[column_index]-min_val)/float(max_val-min_val))
+			new_matrix.append(np.asarray(new_column))
+		return concatenate(new_matrix)
+	else:
+		print("Papatya Normalizer: Given data has to be 1-D or 2-D but given data's dimension is complex for this function.")
+		print("Terminating...")
+		exit()
 
 def plothis(history):
 # "Accuracy"
@@ -111,3 +131,49 @@ def plothis(history):
 	plt.xlabel('epoch')
 	plt.legend(['train', 'validation'], loc='upper left')
 	plt.show()
+
+def nastats(data):
+	'''
+	This function gives stats of nan values of given data. It just prints it does not return anything.
+	Implemented data types:
+		numpy.ndarray (with 1-D or 2-D)
+	'''
+	total_rows_include = 0
+	num_columns_include = []
+	data_isnan = np.isnan(data)
+	for i in range(data_isnan.shape[1]):
+		num_columns_include.append(0)
+	for i in range(data_isnan.shape[0]):
+		if(type(data_isnan[i]) == np.ndarray):
+			if(data_isnan[i].any()):
+				total_rows_include = total_rows_include+1
+			for v in range(data_isnan.shape[1]):
+				if(data_isnan[i][v]):
+					num_columns_include[v] = num_columns_include[v]+1
+		else:
+			# TODO: Handle the 1-D numpy.ndarray
+			pass
+	print("=====================NASTATS=====================")
+	print("Total rows include nan:",total_rows_include)
+	print("Columns's counts:")
+	for i in range(len(num_columns_include)):
+		print("\t",i,":",num_columns_include[i])
+	print("=================================================")
+	print("\n")
+
+def natomedian(data):
+	'''
+	This function replace nan values with median value.
+	'''
+	data_isnan = np.isnan(data)
+	median_val = np.nanmedian(data)
+	print("median val:",median_val)
+	for i in range(data_isnan.shape[0]):
+		if(type(data_isnan[i]) == np.ndarray):
+			for v in range(data_isnan.shape[1]):
+				if(data_isnan[i][v]):
+					data[i][v] = median_val
+		else:
+			# TODO: Handle the 1-D numpy.ndarray
+			pass
+	return data
